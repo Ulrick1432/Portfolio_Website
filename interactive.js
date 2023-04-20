5b45544c009b0f112d0a003c509ee5fdd5d8f583
Ulrick Preben Hansen <ulrick.preben.hansen@live.dk>
2023-04-20 19:13:34 +0200

    list-style: none; added to <summary>

--- interactive.js
+++ interactive.js
@@ -1,6 +1,6 @@
 //Enable dropdown menus
-//Let users toggle between light and dark mode
-/*alert("det virker");*/
+//Let users toggle between light and dark mode https://dcodemania.com/post/switch-dark-light-mode-css-javascript
+//alert("det virker");
 
 function hide() {
     
