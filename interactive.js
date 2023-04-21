a8522acd2fb677482ba5915493510576cbf06a48
Ulrick Preben Hansen <ulrick.preben.hansen@live.dk>
2023-04-21 13:24:09 +0200

    comments about making dark mode

--- interactive.js
+++ interactive.js
@@ -1,7 +1,14 @@
 //Enable dropdown menus
 //Let users toggle between light and dark mode https://dcodemania.com/post/switch-dark-light-mode-css-javascript
 //alert("det virker");
+const modeBtn = document.getElementById('dark_mode_button');
+const body = document.querySelector('body');
 
-function hide() {
-    
+//nedenstående function tilføjer en class til et element, når der klikkes
+// på mode knappen.
+modeBtn.onclick = () => {
+    modeBtn.classList.toggle('active');
+    body.classList.toggle('active');
+
 }
+
