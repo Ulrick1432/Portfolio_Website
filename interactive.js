70e2075a3f0682dd24fc30ae57a4c322875d111d
Ulrick Preben Hansen <ulrick.preben.hansen@live.dk>
2023-04-26 10:09:12 +0200

    small changes

--- interactive.js
+++ interactive.js
@@ -2,13 +2,11 @@
 //Let users toggle between light and dark mode https://dcodemania.com/post/switch-dark-light-mode-css-javascript
 //alert("det virker");
 const modeBtn = document.getElementById('dark_mode_button');
-const body = document.querySelector('body');
 
 //nedenstående function tilføjer en class til et element, når der klikkes
 // på mode knappen.
 modeBtn.onclick = () => {
-    modeBtn.classList.toggle('active');
-    body.classList.toggle('active');
-
+    modeBtn.classList.toggle('dark-mode');
 }
+
 
